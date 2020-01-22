12c3099ae1e7bd9880d58d4a4473b66cd5c5ae31
Timothy J. Beck <timothyjrbeck@gmail.com>
2020-01-22 20:32:07 +0100

    Update control.js

--- node-server/sockets/control.js
+++ node-server/sockets/control.js
@@ -4,12 +4,12 @@ module.exports = function(io){
     
         socket.on('keyboard', function (data) {
             console.log('keyboard: ' + data)
-            // control.emit('keyboard', data)
+            control.emit('keyboard', data)
         });
 
         socket.on('sensor', function (data) {
             console.log('sensor: ' + data)
-            // control.emit('keyboard', data)
+            control.emit('sensor', data)
         });
     
         socket.on('disconnect', function () {
